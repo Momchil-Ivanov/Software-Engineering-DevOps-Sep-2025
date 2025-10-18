71aa776da796d5a4db7456529e0b7f38173118ff
Momchil Ivanov <momchil2207@gmail.com>
2025-10-18 19:05:58 +0300

    Fix about page test timeout - increase to 10 seconds

--- tests/about.tests.js
+++ tests/about.tests.js
@@ -3,6 +3,7 @@ const fetch = require('node-fetch');
 
 suite('About page', function() {
   test('Page title', async function() {
+    this.timeout(10000); // 10 seconds instead of 3
     let res = await fetch("http://localhost:8888/about");
     let body = await res.text();
     assert.ok(body.includes("<title>About</title>"));
